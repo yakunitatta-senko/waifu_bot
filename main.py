a06bcaec46b7a96539f17a4b53ec4e777e8fa468
フォクシー | Senko#4703 <73608888+yakunitatta-senko@users.noreply.github.com>
2023-09-22 00:25:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import aiohttp
 import asyncio
 
 
+import json
+
 from waifu import WaifuClient
 client = WaifuClient()
 
@@ -33,6 +35,21 @@ bot = commands.Bot(command_prefix=",",intents =intents)
 
 # Dictionary to track reactions on messages
 reacted_messages = {}
+
+#point Dictionary
+user_points = {}
+
+image_url_channel_id = 1152252459629154474  # Replace with the actual channel ID
+
+# JSON file to store user points
+points_file = 'user_points.json'
+
+# Load user points from the JSON file (if it exists)
+try:
+    with open(points_file, 'r') as file:
+        user_points = json.load(file)
+except FileNotFoundError:
+    pass
 
 
 @bot.event
@@ -60,12 +77,34 @@ async def w(ctx):
     try:
             # Wait for a reaction to the embed for up to 10 seconds
             reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=lambda r, u: u != bot.user and str(r.emoji) == '✅')
+
+            # Collect the URL of the big bottom image
+            image_url = embed.thumbnail.url if embed.thumbnail else None
+
+            # Award points to the user
+            user_id = str(user.id)
+            user_points[user_id] = user_points.get(user_id, 0) + 1
+
+            # Send a message with the user's points and the image URL
+            points_message = f'{user.mention} has {user_points[user_id]} points!'
+            if image_url:
+                points_message += f'\nImage URL: {image_url}'
+            await ctx.send(points_message)
     except asyncio.TimeoutError:
             # If no reaction is added within 10 seconds, break the loop
             break
 
         # If a reaction was added, continue to the next iteration of the loop
-    await.send("OwO Loading More Wiafu's")
+    continue
+    try:
+            # Wait for a reaction to the embed for up to 10 seconds
+            reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=lambda r, u: u != bot.user and str(r.emoji) == '✅')
+    except asyncio.TimeoutError:
+            # If no reaction is added within 10 seconds, break the loop
+            break
+
+        # If a reaction was added, continue to the next iteration of the loop
+    await ctx.send("OwO Loading More Wiafu's")
     time.sleep(20)
     continue
     time.sleep(10)
@@ -85,7 +124,17 @@ async def on_reaction_add(reaction, user):
             else:
                 # Increment the reaction count for this message
                 reacted_messages[reaction.message.id] = reacted_messages.get(reaction.message.id, 0) + 1
+            embed = reaction.message.embeds[0]  
+            image_url = embed.image.url if embed.image else None
+            points_message = f'{user.mention} has {user_points[str(user.id)]} points!'
 
+            if image_url:
+                points_message += f'\nImage URL: {image_url}'
+                # Send the image URL to the specified channel
+                image_url_channel = bot.get_channel(image_url_channel_id)
+                if image_url_channel:
+                    await image_url_channel.send(f"> {user.mention} has claimed")
+                    await image_url_channel.send(image_url)
                 embed = reaction.message.embeds[0]  # Get the existing embed
                 await reaction.message.edit(embed=embed)
                 await reaction.message.channel.send(f'{user.mention} reacted with ✅! So its basiclly theirs now',reference=reaction.message)
@@ -108,6 +157,11 @@ async def on_reaction_remove(reaction, user):
 
 
 
+@bot.event
+async def on_disconnect():
+    with open(points_file, 'w') as file:
+        json.dump(user_points, file)
+    os.system("kill 1")
 
 
 
